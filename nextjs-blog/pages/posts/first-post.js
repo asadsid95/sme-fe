c7b196bfac5cc9eafe02613644f92445e2d28c53
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2023-06-08 02:06:31 -0400

    practice directory created to test nestjs

---
+++ nextjs-blog/pages/posts/first-post.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function secondpage() {
+    return (
+        <>
+            <h1>This is second page!</h1>
+            <Link href="/">back to Roots!</Link>
+        </>
+    )
+}
