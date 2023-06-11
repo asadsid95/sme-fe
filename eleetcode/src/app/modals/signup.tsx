5814158a7cf6066adb81563be31e8b4e63357f4d
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2023-06-11 06:41:24 -0400

    form submission; #1 #2

--- eleetcode/src/app/modals/signup.tsx
+++ eleetcode/src/app/modals/signup.tsx
@@ -1,4 +1,7 @@
 'use client'
+
+import { useState } from "react"
+
 async function submitFirebase(e) {
     e.preventDefault()
 
@@ -9,6 +12,10 @@ async function submitFirebase(e) {
 
 export default function Signup() {
 
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
     return <>
         <div className="z-10 w-full h-screen absolute top-1/4 left-1/2">
             <div className="w-1/5 h-2/5  bg-gradient-to-b from-dark-yellow to-dark-gray-7 rounded-lg border-solid border-white border-2 ">
@@ -18,15 +25,15 @@ export default function Signup() {
                     {/* </div> */}
                     {/* <div> */}
                     <label>Full Name</label>
-                    <input name='name' />
+                    <input name='name' value={name} onChange={e => { setName(e.target.value) }} />
                     {/* </div> */}
                     {/* <div> */}
                     <label>Email</label>
-                    <input name='email' />
+                    <input name='email' value={email} onChange={e => setEmail(e.target.value)} />
                     {/* </div> */}
                     {/* <div> */}
                     <label>Password</label>
-                    <input name="password" />
+                    <input name="password" value={password} onChange={e => setPassword(e.target.value)} />
                     {/* </div> */}
                     {/* <div> */}
                     <button className="bg-gray-200 rounded-md border-solid border-gray-400 border-2" type="submit">Register</button>
