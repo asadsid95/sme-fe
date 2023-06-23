46abd185af1187300c06552f1a07942c8a72cd66
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2023-06-23 12:38:56 -0400

    problems table modified; #4 #21

--- eleetcode/src/app/components/problemsList/listProblems.tsx
+++ eleetcode/src/app/components/problemsList/listProblems.tsx
@@ -1,29 +1,37 @@
 import { problems } from "@/app/utils/problems"
+import Link from "next/link"
+import { BsCheckCircle } from 'react-icons/bs'
+import { AiOutlineSolution } from 'react-icons/ai'
 
 const Problems = () => {
 
     return (
-        <tbody>
+        <tbody className="text-white text-sm">
             {problems.map((problem, index) => {
 
+                const difficultyColour =
+                    problem.difficulty == "Hard" ? 'text-red-400' : problem.difficulty == "Medium" ? "text-yellow-400" : "text-green-400"
 
 
                 return (
-                    <tr key={index} className=''>
-                        <td>
-                            {problem.title}
+                    // alt coloring of row
+                    <tr key={problem.id} className={`${index % 2 == 1 ? 'bg-gray-400' : ''}`}>
+
+                        <td className="px-4 py-3">
+                            <BsCheckCircle fontSize={'20'} className="text-green-700" />
                         </td>
-                        <td>
-                            {problem.title}
+                        <td className="px-4 py-3">
+                            <Link href="/" className="hover:text-blue-500 hover:font-semibold">{problem.title}</Link>
                         </td>
-                        <td>
+                        <td className={`px-4 py-3 ${difficultyColour}`}>
                             {problem.difficulty}
                         </td>
-                        <td>
+                        <td className="px-4 py-3">
                             {problem.category}
                         </td>
-                        <td>
-                            {problem.videoId}
+                        <td className="px-4 py-3 center">
+                            <AiOutlineSolution fontSize={'20'} />
+                            {/* {problem.videoId} */}
                         </td>
 
                     </tr>
