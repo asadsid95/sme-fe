006df750395cc2534b5a09730dbb55d929e29a5d
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2023-07-03 15:54:15 -0400

    Split not visible; #5 #22

--- eleetcode/src/app/(components)/problemsList/[problems]/page.tsx
+++ eleetcode/src/app/(components)/problemsList/[problems]/page.tsx
@@ -1,19 +1,22 @@
+'use client'
+
 import Split from "react-split";
 import Topbar from "../topbar";
 
 export default function SomeProblem({ params }: { params: { problem: String } }) {
 
+
     return (
 
-        <div className="h-screen bg-dark-layer-1">
+        < div className="h-screen bg-dark-gray-1" >
             <Topbar />
 
             {/* 1 split */}
-            <Split
-                className="split"
-                direction="vertical"
+            <Split className="split"
+                direction="horizontal"
+                gutterSize={100}
                 minSize={300}
-                maxSize={1000}>
+            >
                 <div className="flex items-center justify-center bg-dark-layer-2 border-2 border-white border-solid text-white">
                     <div className="border-white border-solid">
                         Problem description
