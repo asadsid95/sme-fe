4513704db026537ec58f43366d82078a1ae35e12
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2023-07-02 23:53:32 -0400

    starter code; #5 #22

--- eleetcode/src/app/(components)/problemsList/[problems]/page.tsx
+++ eleetcode/src/app/(components)/problemsList/[problems]/page.tsx
@@ -1,6 +1,31 @@
-export default function SomeProblem({ params }: { params: { problems: String } }) {
+import Split from "react-split";
+import Topbar from "../topbar";
+
+export default function SomeProblem({ params }: { params: { problem: String } }) {
 
     return (
-        <h1> Hello world This is for {params.problems} </h1>
+
+        <div className="h-screen bg-dark-layer-1">
+            <Topbar />
+
+            {/* 1 split */}
+            <Split
+                className="split"
+                direction="vertical"
+                minSize={300}
+                maxSize={1000}>
+                <div className="flex items-center justify-center bg-dark-layer-2 border-2 border-white border-solid text-white">
+                    <div className="border-white border-solid">
+                        Problem description
+                    </div>
+                    <div className="border-white border-solid">
+                        Code editor
+                    </div>
+
+                </div>
+            </Split>
+
+        </div >
+
     )
 }
