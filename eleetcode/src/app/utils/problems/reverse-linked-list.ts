eaebd892063320e999c8df8ded0d2c46bd144a1f
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2023-07-06 16:05:22 -0400

    add more to local storage of problems; #5 #22 ++ prev commit is related to this

---
+++ eleetcode/src/app/utils/problems/reverse-linked-list.ts
@@ -0,0 +1,93 @@
+import { Problem } from "../types/problem";
+import assert from "assert";
+
+class LinkedList {
+    value: number;
+    next: LinkedList | null;
+
+    constructor(value: number) {
+        this.value = value
+        this.next = null
+    }
+
+    reverse(): LinkedList {
+        let current: LinkedList | null = this;
+        let prev: LinkedList | null = null;
+        while (current !== null) {
+            const next = current.next as LinkedList;
+            current.next = prev;
+            prev = current;
+            current = next
+        }
+        return prev!;
+    }
+}
+
+const starterCodeReverseLinkedListJS = `
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+// Do not edit function name
+function reverseLinkedList(head) {
+  // Write your code here
+};`;
+
+
+export const reverseLinkedListHandler = (fn: any) => {
+    try {
+        const tests = [[1, 2, 3, 4, 5], [5, 4, 3, 2, 1], [1, 2, 3], [1]];
+        const answers = [[5, 4, 3, 2, 1], [1, 2, 3, 4, 5], [3, 2, 1], [1]];
+        for (let i = 0; i < tests.length; i++) {
+            const list = createLinkedList(tests[i]);
+            const result = fn(list);        // Likely represent reverse() of Linked
+            assert.deepEqual(getListValues(result), answers[i]);
+        }
+        return true;
+    } catch (error: any) {
+        console.log("Error from reverseLinkedListHandler: ", error);
+        throw new Error(error);
+
+    }
+}
+
+function createLinkedList(values: number[]) = {
+
+    return ()
+}
+
+function getListValues(head: LinkedList)
+
+export const reverseLinkedList: Problem = {
+    id: "reverse-linked-list",
+    title: "2. Reverse Linked List",
+    problemStatement: `<p class='mt-3'>Given the <code>head</code> of a singly linked list, reverse the list, and return <em>the reversed list</em>.</p>
+	`,
+    examples: [
+        {
+            id: 0,
+            inputText: "head = [1,2,3,4,5]",
+            outputText: "[5,4,3,2,1]",
+            img: example.src,
+        },
+        {
+            id: 1,
+            inputText: "head = [1,2,3]",
+            outputText: "[3,2,1]",
+        },
+        {
+            id: 2,
+            inputText: "head = [1]",
+            outputText: "[1]",
+        },
+    ],
+    constraints: `<li class='mt-2'>The number of nodes in the list is the range <code>[0, 5000]</code>.</li>
+<li class='mt-2'><code>-5000 <= Node.val <= 5000</code></li>`,
+    starterCode: starterCodeReverseLinkedListJS,
+    handlerFunction: reverseLinkedListHandler,
+    starterFunctionName: "function reverseLinkedList(",
+    order: 2,
+};
--- eleetcode/src/app/utils/problems/two-sum.ts
+++ eleetcode/src/app/utils/problems/two-sum.ts
@@ -41,14 +41,14 @@ export const twoSum: Problem = {
 
     id: "two-sum",
     title: "1. Two Sum",
-    problemStatement: ` <p className='mt-3'>
+    problemStatement: ` <p class='mt-3'>
                                 Given an array of integers <code>nums</code> and an integer <code>target</code>, return <em>indices of the two numbers such that they add up to</em> <code>target</code>.
                             </p>
-                            <p className='mt-3'>
+                            <p class='mt-3'>
                                 You may assume that each input would have <strong>exactly one solution</strong>, and you
                                 may not use thesame element twice.
                             </p>
-                            <p className='mt-3'>You can return the answer in any order.</p>`,
+                            <p class='mt-3'>You can return the answer in any order.</p>`,
     examples: [
         {
             id: 1,
@@ -68,17 +68,17 @@ export const twoSum: Problem = {
             outputText: "[0,1]",
         },
     ],
-    constraints: `<li className='mt-2'>
+    constraints: `<li class='mt-2'>
                                     <code>2 ≤ nums.length ≤ 10</code>
                                 </li>
 
-                                <li className='mt-2'>
+                                <li class='mt-2'>
                                     <code>-10 ≤ nums[i] ≤ 10</code>
                                 </li>
-                                <li className='mt-2'>
+                                <li class='mt-2'>
                                     <code>-10 ≤ target ≤ 10</code>
                                 </li>
-                                <li className='mt-2 text-xs'>
+                                <li class='mt-2 text-xs'>
                                     <strong>Only one valid answer exists.</strong>
                                 </li>`,
     order: 1,
